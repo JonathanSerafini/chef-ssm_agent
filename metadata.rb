bd0d0f1fe3268873bc9bcfeebd05f486149ed629
Jonathan Serafini <jonathan@serafini.ca>
2016-12-22 15:32:58 -0500

    Cleanup metadata

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'jonathan@serafini.ca'
 license 'Apache 2.0'
 description 'Amazon EC2 Simple Systems Manager (SSM) agent'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-issues_url       'https://github.com/JonathanSerafini/chef-ssm_agent/issues'
-source_url       'https://github.com/JonathanSerafini/chef-ssm_agent'
+issues_url 'https://github.com/JonathanSerafini/chef-ssm_agent/issues'
+source_url 'https://github.com/JonathanSerafini/chef-ssm_agent'
 version '0.1.0'
